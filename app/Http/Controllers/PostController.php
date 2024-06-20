647806ccc22e5cfa43520e2fb330f995bd850728
mizukitera <m.teranishi1005@gmail.com>
2024-06-20 22:30:00 +0900

    メール投稿の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,18 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
     {
-        return view('posts.index')->with(['posts' => $post->getByLimit()]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }//
+    
     public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);
     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
     }
+    
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,6 +8,7 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        <a href='/posts/create'>create</a>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
@@ -17,6 +18,9 @@
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
